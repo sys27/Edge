44dfe475f1fafcdce9d6dc52beb5e4f51d409471
sys_27 <sys_27@mail.ru>
2013-06-08 20:20:09 +0300

    .

---
+++ Edge/EdgeLexer.cs
@@ -0,0 +1,18 @@
+﻿using Edge.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace Edge
+{
+    
+    public class EdgeLexer : ILexer
+    {
+
+        public IEnumerable<IToken> Tokenize(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ Edge/ILexer.cs
@@ -0,0 +1,15 @@
+﻿using Edge.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace Edge
+{
+
+    public interface ILexer
+    {
+
+        IEnumerable<IToken> Tokenize(string text);
+
+    }
+
+}
