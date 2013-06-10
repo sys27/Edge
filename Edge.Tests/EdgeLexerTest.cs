48a4b33cb903a579c0d2f75372f3b4a4a60cba8c
sys_27 <sys_27@mail.ru>
2013-06-10 15:29:15 +0300

    Added IdToken

---
+++ Edge.Tests/EdgeLexerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Edge;
+
+namespace Edge.Tests
+{
+
+    [TestClass]
+    public class EdgeLexerTest
+    {
+
+        private ILexer lexer = new EdgeLexer();
+
+    }
+
+}
---
+++ Edge/Tokens/IdToken.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Edge.Tokens
+{
+    
+    public class IdToken : IToken
+    {
+
+        private string id;
+
+        public IdToken(string id)
+        {
+            this.id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var token = obj as IdToken;
+            if (token != null || token.id == this.id)
+                return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + id;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+    }
+
+}
