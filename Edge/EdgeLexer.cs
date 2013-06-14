41d6c23eda9119db32aaba05df70a885c65c3f15
sys_27 <sys_27@mail.ru>
2013-06-14 09:48:12 +0300

    .

--- Edge/EdgeLexer.cs
+++ Edge/EdgeLexer.cs
@@ -4,13 +4,20 @@ using System.Collections.Generic;
 
 namespace Edge
 {
-    
+
     public class EdgeLexer : ILexer
     {
 
         public IEnumerable<IToken> Tokenize(string text)
         {
-            throw new NotImplementedException();
+            var tokens = new List<IToken>();
+
+            for (int i = 0; i < text.Length; )
+            {
+
+            }
+
+            return tokens;
         }
 
     }
