0fdc6547d4dac7dc78aec75c2d033c40a9d7e2c9
Dmitry <sys_27@mail.ru>
2013-08-04 10:10:31 +0300

    .

--- Edge/EdgeParser.cs
+++ Edge/EdgeParser.cs
@@ -2,6 +2,8 @@
 using Edge.Tokens;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Edge
 {
@@ -11,7 +13,8 @@ namespace Edge
 
         private ILexer lexer;
 
-        private IEnumerable<IToken> tokens;
+        private List<IToken> tokens;
+        private int position;
 
         public EdgeParser()
             : this(new EdgeLexer())
@@ -24,6 +27,24 @@ namespace Edge
             this.lexer = lexer;
         }
 
+        private IToken GetToken()
+        {
+            var token = tokens[position];
+            position++;
+
+            return token;
+        }
+
+        private IToken Peek()
+        {
+            return tokens[position];
+        }
+
+        private Type SearchType(string type)
+        {
+            throw new NotImplementedException();
+        }
+
         private RootNode Root()
         {
             var namespaces = Namespaces();
@@ -34,15 +55,96 @@ namespace Edge
 
         private IEnumerable<NamespaceNode> Namespaces()
         {
-            throw new NotImplementedException();
+            if (Peek() is UsingToken)
+            {
+                var namespaces = new List<NamespaceNode>();
+
+                while (GetToken() is UsingToken)
+                {
+                    namespaces.Add(GetNamespace());
+                }
+
+                return namespaces;
+            }
+
+            return null;
+        }
+
+        private NamespaceNode GetNamespace()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (true)
+            {
+                var token = GetToken();
+                if (token is SymbolToken && ((SymbolToken)token).Symbol == ';')
+                    break;
+
+                if (token is WordToken)
+                    sb.Append(((WordToken)token).Word);
+                else if (token is SymbolToken && ((SymbolToken)token).Symbol == '.')
+                    sb.Append('.');
+                else
+                    // todo: error message 
+                    throw new EdgeParserException();
+            }
+
+            var ns = sb.ToString();
+            if (string.IsNullOrWhiteSpace(ns))
+                // todo: error message
+                throw new EdgeParserException();
+
+            return new NamespaceNode(ns);
         }
 
         private ObjectNode Object()
         {
-            throw new NotImplementedException();
+            var token = GetToken();
+            if (!(token is TypeToken))
+                // todo: error message
+                throw new EdgeParserException();
+
+            var type = SearchType(((TypeToken)token).Type);
+            //var ctor = Constructor();
+            var properties = Properties();
+
+            return new ObjectNode(type, properties);
         }
 
         private IEnumerable<PropertyNode> Properties()
+        {
+            var token = Peek();
+            if (token is SymbolToken && ((SymbolToken)token).Symbol == '{')
+            {
+                position++;
+                var properties = new List<PropertyNode>();
+
+                while (true)
+                {
+                    token = Peek();
+                    if (token is SymbolToken && ((SymbolToken)token).Symbol == '}')
+                    {
+                        position++;
+                        break;
+                    }
+
+                    if (token is PropertyToken)
+                        properties.Add(GetProperty());
+                    else
+                        // todo: error message
+                        throw new EdgeParserException();
+                }
+
+                if (properties.Count == 0)
+                    return null;
+
+                return properties;
+            }
+
+            return null;
+        }
+
+        private PropertyNode GetProperty()
         {
             throw new NotImplementedException();
         }
@@ -52,9 +154,9 @@ namespace Edge
             if (string.IsNullOrWhiteSpace(text))
                 throw new ArgumentNullException("text");
 
-            tokens = lexer.Tokenize(text);
+            tokens = lexer.Tokenize(text).ToList();
 
-            throw new NotImplementedException();
+            return Root();
         }
 
     }
---
+++ Edge/EdgeParserException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Edge
+{
+
+    [Serializable]
+    public class EdgeParserException : Exception
+    {
+
+        public EdgeParserException() { }
+
+        public EdgeParserException(string message) : base(message) { }
+
+        public EdgeParserException(string message, Exception inner) : base(message, inner) { }
+
+        protected EdgeParserException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+
+    }
+
+}
