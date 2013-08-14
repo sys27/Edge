60dd25a287b1d0e1bbe3c6c3a8852def1a77bb89
Dmitry <sys_27@mail.ru>
2013-08-14 18:06:51 +0300

    .

---
+++ Edge/IAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿using Edge.SyntaxNodes;
+using System;
+using System.Collections.Generic;
+namespace Edge
+{
+    
+    public interface IAnalyzer
+    {
+
+        RootNode MatchTypes(RootNode rootNode);
+
+    }
+
+}
