79521db9ed634301db584fee0fe613baa02b0a1c
sys_27 <sys_27@mail.ru>
2013-09-01 11:23:35 +0300

    .

---
+++ Edge.CustomTool/EdgeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TextTemplating.VSHost;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Edge.CustomTool
+{
+
+    [ComVisible(true)]
+    [Guid("7092B522-44B0-4260-B92B-903ABC512A42")]
+    public class EdgeGenerator : BaseCodeGeneratorWithSite
+    {
+
+        protected override byte[] GenerateCode(string inputFileName, string inputFileContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetDefaultExtension()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
