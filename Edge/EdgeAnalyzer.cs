e5d4dfb13e9f68729167c383e61b0c42e5b52cd1
sys_27 <sys_27@mail.ru>
2013-09-04 21:14:44 +0300

    .

--- Edge/EdgeAnalyzer.cs
+++ Edge/EdgeAnalyzer.cs
@@ -18,6 +18,29 @@ namespace Edge
 
         }
 
+        private IEnumerable<Type> GetAppropriateTypes(string type)
+        {
+            return from lib in assemblies
+                   let assembly = Assembly.Load(lib)
+                   from t in assembly.GetTypes()
+                   from ns in namespaces
+                   where t.Namespace == ns && t.Name == type
+                   select t;
+        }
+
+        private Type CheckType(string type)
+        {
+            var types = GetAppropriateTypes(type);
+
+            // todo: error message
+            if (types.Count() == 0)
+                throw new EdgeAnalyzerException();
+            if (types.Count() > 1)
+                throw new EdgeAnalyzerException();
+
+            return types.First();
+        }
+
         public void Analyze(SyntaxTree tree)
         {
             ChechNamespaces(tree.Namespaces.Concat(namespaces).Distinct());
@@ -49,13 +72,30 @@ namespace Edge
 
         private void ChechObjects(IEnumerable<ObjectNode> objects)
         {
+            ChechAllIDs(objects);
+
             foreach (var obj in objects)
                 CheckObject(obj);
         }
 
+        private void ChechAllIDs(IEnumerable<ObjectNode> objects)
+        {
+            var list = objects.ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i].Id == list[j].Id)
+                        // todo: error message
+                        throw new EdgeAnalyzerException();
+                }
+            }
+        }
+
         private void CheckObject(ObjectNode obj)
         {
-
+            var type = CheckType(obj.Type);
         }
 
         public IEnumerable<string> Assemblies
